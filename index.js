d3ae2738eafc4b0576263d975718c24b7a70261b
Audy0714 <audykei7907@gmail.com>
2021-03-17 15:36:18 +0100

    remove date from registration

--- index.js
+++ index.js
@@ -6,36 +6,11 @@ const pg = require('pg');
 
 const app = express();
 
-const session = require('express-session');
-
 const cors = require('cors');
 
 const port = process.env.PORT || 5478;
 
 const router = require('./app/router');
-
-
-// mise en place et configuration de la session
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: 'odok',
-    cookie: {
-        secure: false,
-        maxAge: 60 * 60 * 10000 // une heure
-    }
-}));
-
-// rendre disponible dans toutes les vues, l'éventuel utilisateur connecté
-app.use((req, res, next) => {
-    // si un utilisateur est connecté
-    if (req.session.user) {
-        // on le rend disponible dans les views
-        res.locals.user = req.session.user;
-    }
-
-    next();
-});
 
 app.use(cors());
 
