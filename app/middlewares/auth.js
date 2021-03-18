e6523b947461ed49f1dc32a7858873385f3e06a4
Audy0714 <audykei7907@gmail.com>
2021-03-18 14:16:35 +0100

    error in auth

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -2,25 +2,25 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
-function auth(request, response, next) {
+function auth(req, res, next) {
 	// je le reçois du coté front
-	const authHeader = request.headers.authorization;
+	const authHeader = req.headers.authorization;
 	// token d'authentification reçu lors de la connexion de l'utilisateur (sécurité)
 	const token = authHeader && authHeader.split(' ')[1];
 
-	if (!token) return response.status(401).json('Access denied. No token provided');
+	if (!token) return res.status(401).json('Access denied. No token provided');
 
 	try {
 
 		const decodedPayload = jwt.verify(token, process.env.JWTPRIVATEKEY);
 
-		request.user = decodedPayload;
+		req.user = decodedPayload;
 
 		next();
 
 	} catch (error) {
 
-		response.status(400).json('Invalid token.');
+		res.status(400).json('Invalid token.');
 	}
 };
 
